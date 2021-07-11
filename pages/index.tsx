a89d66afea44ed7e0bd98302aecba86cd6d27b60
FadhilP <fadhilpradipta0@gmail.com>
2021-07-11 23:09:36 +0700

    Update home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
 import { Fragment } from "react";
 
+import ProjectCard from "@/components/pages/home/ProjectCard";
+import { Projects, PersonalProjects } from "@/components/pages/home/ProjectCard/ProjectContent";
+import Header from "@/components/pages/home/Header";
+
 export default function Home() {
   return (
     <Fragment>
-      <div className="m-12">
-        <div className="flex flex-wrap justify-between">
-          <div>Test</div>
-          <div>Project One</div>
+      <div className="m-12 mx-48 select-none">
+        <Header>Projects that I've worked on</Header>
+        <div className="flex flex-wrap mt-8 justify-center">
+          {Projects.map((project) => (
+            <ProjectCard {...project} />
+          ))}
+        </div>
+        <div className="text-center tracking-widest mt-12">
+          <Header delay={0.5}>Personal Projects</Header>
+        </div>
+        <div className="flex flex-wrap mt-8 justify-center">
+          {PersonalProjects.map((project) => (
+            <ProjectCard {...project} />
+          ))}
         </div>
       </div>
     </Fragment>
