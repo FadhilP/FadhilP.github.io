a1f098f55251f01034706032b9126a74b1274e70
FadhilP <fadhilpradipta0@gmail.com>
2021-07-11 19:24:51 +0700

    Fix tailwind build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import { Fragment } from "react";
 export default function Home() {
   return (
     <Fragment>
-      Test
+      <div className="m-12">
+        <div className="flex flex-wrap justify-between">
+          <div>Test</div>
+          <div>Project One</div>
+        </div>
+      </div>
     </Fragment>
-  )
+  );
 }
