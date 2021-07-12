fb1c3416deb035e6cf60c02799e6b7bb57fb6f8d
FadhilP <45789668+FadhilP@users.noreply.github.com>
2021-07-12 17:15:31 +0000

    Deploying to gh-pages from @ FadhilP/FadhilP.github.io@4f848863a6031dd9f53b2662762b7c1aec24422f 🚀

---
+++ _next/static/hKFB_AC3Z9-wsa42xKdMU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/843-fa3ade098da73ac391cf.js","static/chunks/pages/index-9727c8dbaa99f692c9e6.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/qobj9MIJh-YjRfcGGNZed/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/843-fa3ade098da73ac391cf.js","static/chunks/pages/index-256bc2a9bd4893a1f96b.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
