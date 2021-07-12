65e5d4c341846da49e661afc5b0801635b75b3d9
FadhilP <45789668+FadhilP@users.noreply.github.com>
2021-07-12 16:52:21 +0000

    Deploying to gh-pages from @ FadhilP/FadhilP.github.io@be897a007660b5fcfc664c11443c92bda0008243 🚀

--- _next/static/uwNo-jYu1LQ7eLc1fnpjL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/843-fa3ade098da73ac391cf.js","static/chunks/pages/index-8a8175c58d3f06b5edf3.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
